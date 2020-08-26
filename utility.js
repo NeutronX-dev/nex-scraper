b5747ff9dd310366743d1db9a27fa10d56241120
NeutronX-dev <32855909+NeutronX-dev@users.noreply.github.com>
2020-08-26 10:19:59 -0400

    Added forEach Callback.
    Execute a callback for each proxy it Detects.

--- utility.js
+++ utility.js
@@ -1,38 +1,38 @@
-const _0x170f = ['match', 'removeDuplicates', 'request', 'filter', 'test', 'isProxy', 'filterProxies', 'isHTML', 'replace', 'indexOf', 'object', 'exports', 'toString', '\x27.removeDuplicates(proxies)\x27:\x20\x22proxies\x22\x20is\x20not\x20an\x20Array.', 'string', 'replaceAll', 'prototype', '\x27.filterProxies(text)\x27:\x20\x22text\x22\x20is\x20not\x20an\x20String.'];
-(function (_0xb32659, _0x170f0b) {
-    const _0x2b3b7f = function (_0x20ba98) {
-        while (--_0x20ba98) {
-            _0xb32659['push'](_0xb32659['shift']());
+const _0x2012 = ['\x27.removeDuplicates(proxies)\x27:\x20\x22proxies\x22\x20is\x20not\x20an\x20Array.', 'isHTML', 'request', 'filterProxies', 'replace', 'replaceAll', 'match', '$$$$', 'toString', 'indexOf', 'isProxy', '\x27.filterProxies(text)\x27:\x20\x22text\x22\x20is\x20not\x20an\x20String.', 'filter', 'removeDuplicates', '\x5c$&', 'string', 'test', 'prototype', 'object', 'exports'];
+(function (_0xd15d55, _0x2012dc) {
+    const _0x132b82 = function (_0x2cbe44) {
+        while (--_0x2cbe44) {
+            _0xd15d55['push'](_0xd15d55['shift']());
         }
     };
-    _0x2b3b7f(++_0x170f0b);
-}(_0x170f, 0x93));
-const _0x2b3b = function (_0xb32659, _0x170f0b) {
-    _0xb32659 = _0xb32659 - 0x0;
-    let _0x2b3b7f = _0x170f[_0xb32659];
-    return _0x2b3b7f;
+    _0x132b82(++_0x2012dc);
+}(_0x2012, 0x9a));
+const _0x132b = function (_0xd15d55, _0x2012dc) {
+    _0xd15d55 = _0xd15d55 - 0x0;
+    let _0x132b82 = _0x2012[_0xd15d55];
+    return _0x132b82;
 };
-const request = require(_0x2b3b('0x11')),
+const request = require(_0x132b('0x8')),
     util = class proxy_scraper {
-        constructor() {} [_0x2b3b('0x10')](_0x27c89a) {
-            if (typeof _0x27c89a == _0x2b3b('0x7')) return _0x27c89a[_0x2b3b('0x0')](function (_0x459ad1, _0x3f7651, _0x1c3681) {
-                return _0x1c3681[_0x2b3b('0x6')](_0x459ad1) == _0x3f7651;
+        constructor() {} [_0x132b('0x13')](_0x532cab) {
+            if (typeof _0x532cab == _0x132b('0x4')) return _0x532cab[_0x132b('0x12')](function (_0x370cc6, _0xace02d, _0x4d6841) {
+                return _0x4d6841[_0x132b('0xf')](_0x370cc6) == _0xace02d;
             });
-            else throw new Error(_0x2b3b('0xa'));
-        } [_0x2b3b('0x4')](_0x5ae6cb) {
-            return typeof _0x5ae6cb == _0x2b3b('0xb') ? /<("[^"]*"|'[^']*'|[^'">])*>/img [_0x2b3b('0x1')](_0x5ae6cb) ? !![] : ![] : ![];
-        } [_0x2b3b('0x2')](_0x5920db, _0x461fbc) {
-            return _0x461fbc[_0x2b3b('0x1')]('' + _0x5920db[_0x2b3b('0x9')]()) ? !![] : ![];
-        } [_0x2b3b('0x3')](_0x1f30cf, _0x2e4189) {
-            if (typeof _0x1f30cf == _0x2b3b('0xb')) {
-                let _0x38c75f = _0x1f30cf,
-                    _0x42896b = _0x2e4189;
-                return _0x38c75f[_0x2b3b('0xf')](_0x42896b);
-            } else throw new Error(_0x2b3b('0xe'));
-        } [_0x2b3b('0xc')](_0x51983a, _0x494f8e, _0x3f6773) {
-            String[_0x2b3b('0xd')][_0x2b3b('0xc')] = function (_0x12b57f, _0x235e4f, _0x11e209) {
-                return this['replace'](new RegExp(_0x12b57f[_0x2b3b('0x5')](/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g, '\x5c$&'), _0x11e209 ? 'gi' : 'g'), typeof _0x235e4f == 'string' ? _0x235e4f[_0x2b3b('0x5')](/\$/g, '$$$$') : _0x235e4f);
-            }, _0x51983a['replaceAll'](_0x494f8e, _0x3f6773);
+            else throw new Error(_0x132b('0x6'));
+        } [_0x132b('0x7')](_0x35e913) {
+            return typeof _0x35e913 == _0x132b('0x1') ? /<("[^"]*"|'[^']*'|[^'">])*>/img [_0x132b('0x2')](_0x35e913) ? !![] : ![] : ![];
+        } [_0x132b('0x10')](_0x3742e3, _0x1faf25) {
+            return _0x1faf25[_0x132b('0x2')]('' + _0x3742e3[_0x132b('0xe')]()) ? !![] : ![];
+        } [_0x132b('0x9')](_0x2a0252, _0x574318) {
+            if (typeof _0x2a0252 == _0x132b('0x1')) {
+                let _0x1d5e0f = _0x2a0252,
+                    _0x5cd61a = _0x574318;
+                return _0x1d5e0f[_0x132b('0xc')](_0x5cd61a);
+            } else throw new Error(_0x132b('0x11'));
+        } [_0x132b('0xb')](_0x35b860, _0x7a78b, _0x41c102) {
+            String[_0x132b('0x3')][_0x132b('0xb')] = function (_0x19b6ac, _0x34487a, _0x3c6710) {
+                return this[_0x132b('0xa')](new RegExp(_0x19b6ac[_0x132b('0xa')](/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g, _0x132b('0x0')), _0x3c6710 ? 'gi' : 'g'), typeof _0x34487a == _0x132b('0x1') ? _0x34487a['replace'](/\$/g, _0x132b('0xd')) : _0x34487a);
+            }, _0x35b860[_0x132b('0xb')](_0x7a78b, _0x41c102);
         }
     };
-module[_0x2b3b('0x8')] = new util();
+module[_0x132b('0x5')] = new util();
